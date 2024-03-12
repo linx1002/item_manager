438a5fdf3b3772d742cdf8e3ccb4d26962c85e82
linx1002 <157979525+linx1002@users.noreply.github.com>
2024-03-12 18:18:02 +0900

    Lesson1実装完了

---
+++ src/main/java/com/example/controller/ItemController.java
@@ -0,0 +1,53 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.form.ItemForm;
+
+@Controller
+@RequestMapping("/item")
+public class ItemController {
+	//商品一覧表示
+	@GetMapping
+	public String index(Model model) {
+		return "item/index";
+	}
+	
+	//商品登録ページ表示
+	@GetMapping("toroku")
+	public String torokuPage(@ModelAttribute("itemForm") ItemForm itemForm) {
+		return "item/torokuPage";
+	}
+	
+	//商品登録実行
+	@PostMapping("toroku")
+	public String toroku(ItemForm itemForm) {
+		return "redirect:/item";
+	}
+	
+	//商品編集ページ
+	@GetMapping("henshu/{id}")
+	public String henshuPage(@PathVariable("id") Integer id, Model model
+	,@ModelAttribute("itemForm") ItemForm itemForm) {
+		return "item/henshuPage";
+	}
+	
+	//商品編集の実行
+	@PostMapping("henshu{id}")
+	public String henshu(@PathVariable("id") Integer id,@ModelAttribute("itemForm") ItemForm itemForm) {
+		return "redirect:/item";
+	}
+	
+	//商品削除の実行
+	@PostMapping("sakujo/{id}")
+	public String sakujo(@PathVariable("id") Integer id) {
+		return "redirect:/item";
+	}
+	
+}
